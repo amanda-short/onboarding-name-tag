097d72ec2e7dd689ec9417fc818ea6d4b52f7c99
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-09-07 16:33:55 -0700

    added JS, updated html

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// import functions and grab DOM elements
-
-// let state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const nameInput = document.getElementById('name-input');
+const changeButton = document.getElementById('button');
+const nameDisplay = document.getElementById('name-display');
+changeButton.addEventListener('click', () => {
+  const name = nameInput.value;
+  nameDisplay.textContent = name;
+  nameInput.value = '';
+});
